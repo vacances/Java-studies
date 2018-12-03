67279c1c81d00fc392ec61ce34a8f0aeb7e45c0c
Svietek <piotr.glowala.eng@gmail.com>
2018-12-03 17:15:17 +0100

    Update testpesel.java

--- testpesel.java
+++ testpesel.java
@@ -3,11 +3,11 @@ public class testpesel {
 	int wynik;
 	Pesel p1= new Pesel("86041404054");
 	wynik=p1.spr();
-	System.out.println("Cyfra kontrolna numeru pesel "+p1.pesel+" "+wynik);
+	System.out.println("Cyfra kontrolna numeru pesel "+ p1.getp()+" = "+wynik);
     }
 }
 class Pesel{
-    	String pesel;
+    	private String pesel;
     	Pesel(String numer){
     		pesel=numer;
     	}
@@ -17,15 +17,26 @@ class Pesel{
     	int cyfry[] = new int[11];
     	int wagi[]={1,3,7,9,1,3,7,9,1,3,1};
     	if(pesel.length()!=11) cyfraK=11;
-    	else{
+    	else
+    		try
+    			{
     		int suma=0;
     		for(int i=0;i<11;i++){
-    			cyfry[i]=3;
+    			cyfry [i]=Integer.parseInt(pesel.substring(i,i+1));
+
     			suma=suma+cyfry[i]*wagi[i];
+
+
     		}
     		cyfraK=suma%10;
     	}
+    	catch(NumberFormatException er)
+    	{cyfraK=13;
+    	}
     	return cyfraK;
     	}
-
+String
+getp(){
+	return pesel;
+}
 }
